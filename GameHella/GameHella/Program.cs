7f7fbc89026023bfcf9cab1c9c2590670cfb5030
pushistost <info@pushinistost>
2020-11-07 14:20:31 +0300

    cozdanie draw

--- GameHella/GameHella/Program.cs
+++ GameHella/GameHella/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace GameHella
 {
     class Program
     {
+
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Game!");
+
+            Point p1 = new Point();
+            p1.x = 6;
+            p1.y = 7;
+            p1.sym = '3';
+            Draw(p1.x, p1.y, p1.sym);
+
+
+            Draw(1, 3, '*');
+            Draw(5, 5, '#');
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
