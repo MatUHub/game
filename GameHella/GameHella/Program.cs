a78e7b320fa5a70f97d5e6d6469616de3171920a
pushistost <info@pushinistost>
2020-11-07 13:30:32 +0300

    gameHella

---
+++ GameHella/GameHella/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameHella
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Game!");
+        }
+    }
+}
